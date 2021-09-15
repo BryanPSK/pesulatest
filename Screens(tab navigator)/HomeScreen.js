21c4342f110a07fd9916e3ff5b69522b403bdaf9
BryanPSK <48677467+BryanPSK@users.noreply.github.com>
2021-09-15 16:31:02 +0800

    firebase read
    added firebase read on homescreen

--- Screens(tab navigator)/HomeScreen.js
+++ Screens(tab navigator)/HomeScreen.js
@@ -1,12 +1,43 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-
+import { Text, View, Button} from 'react-native';
+// import { firebase } from "@firebase/app";
+// import "firebase/firestore";
+// import firebase from 'firebase/app';
+import 'firebase/firestore';
+import firebase from '../firebase/firebaseDB';
 
 
 export default function HomeScreen() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Homescreen</Text>
+        <Button title = "firestore" onPress={()=> testwrite } />
       </View>
   );
   }
+
+//   function testwrite(){
+
+  
+//   firebase.firestore()
+//   .collection('SaracaHall')
+//   .add({
+//     title:"Test",
+//     potate : true,
+//   })
+  
+// };
+
+
+  firebase
+  .firestore()
+  .collection("Users3")
+  .doc("mydoc")
+  .collection("Activities")
+  .doc("Database")
+  .set({
+    key: "1",
+    value: "",
+  })
+  .then((ref) => { console.log(ref) });
+
---
+++ firebase/firebaseDB.js
@@ -0,0 +1,16 @@
+import { firebase } from "@firebase/app";
+import "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAY-6k6SxNBKk1APlo-zuajHZoK4S_4dcM",
+    authDomain: "pesulafirebase.firebaseapp.com",
+    databaseURL: "https://pesulafirebase-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "pesulafirebase",
+    storageBucket: "pesulafirebase.appspot.com",
+    messagingSenderId: "58120148635",
+    appId: "1:58120148635:web:085a99d0876d2ec151d83e"
+  };
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
