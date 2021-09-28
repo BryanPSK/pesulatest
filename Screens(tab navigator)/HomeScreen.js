7f399771a0e03ec9f6fc3a6ee68b69a7a5821067
BryanPSK <48677467+BryanPSK@users.noreply.github.com>
2021-09-28 21:36:26 +0800

    added bookmachine1 logic if avail. clarified booking functions

--- Screens(tab navigator)/HomeScreen.js
+++ Screens(tab navigator)/HomeScreen.js
@@ -10,46 +10,73 @@ export default function HomeScreen() {
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Homescreen</Text>
         <Button title = "firestore" onPress={ UpdateFirestore } />
-        <Button title = "readbooking" onPress={ Readbooking } />
-        <Button title = "checktrue" onPress={ checktrue } />
+        <Button title = "checkbookingavailability" onPress={ Readbooking } />
+        <Button title = "bookmachine1" onPress={ checktrue } />
+        <Button title = "forcebookmachine1" onPress={ bookmachine1 } />
+        <Button title = "forceunbookmachine1" onPress={ unbookmachine1 } />
         {/* <Button title = "consolelog" onPress={ ()=>  } /> */}
       </View>
   );
   }
 
+   //this function writes data to firestore 
+  function UpdateFirestore(){
+    firebase.firestore()
+      .collection('SaracaHall')
+      .add({
+        title:"Test1",
+        potate : true,
+      })
+    }
+    
 
- //this function writes data to firestore 
-function UpdateFirestore(){
+//hardcoded function to book machine 1.updates the 'isbooked' boolean field in firestore
+function bookmachine1(){
 firebase.firestore()
   .collection('SaracaHall')
-  .add({
-    title:"Test1",
-    potate : true,
+  .doc('Machine1')
+  .collection('bookstatus')
+  .doc('bookstatus')
+  .set({
+    isbooked: true,
   })
 }
+//function to unbook machine 1. updates the 'isbooked' boolean field in firestore.
+function unbookmachine1(){
+  firebase.firestore()
+    .collection('SaracaHall')
+    .doc('Machine1')
+    .collection('bookstatus')
+    .doc('bookstatus')
+    .set({
+      isbooked: false,
+    })
+  }
 
 //Check if machine is booked or not. then carry out action(in this case print statement)
-function checktrue(){
-  firebase.firestore()
-  .collection('SaracaHall')
-  .doc('Machine1')
-  .collection('bookstatus')
-  .doc('bookstatus')
-  .get()
-  .then( documentSnapshot =>{
-    var isbooked=documentSnapshot.get('isbooked?')
-    if (isbooked == true){
-      //do something
-    }
-    if(isbooked == false){
-      //do something
-    }
+// function checktrue(){
+//   firebase.firestore()
+//   .collection('SaracaHall')
+//   .doc('Machine1')
+//   .collection('bookstatus')
+//   .doc('bookstatus')
+//   .get()
+//   .then( documentSnapshot =>{
+//     var isbooked=documentSnapshot.get('isbooked')
+//     if (isbooked == true){
+//       console.log('machine is currently booked')
+//       //do something
+//     }
+//     if(isbooked == false){
+//       console.log('machine is available for booking')
+//       //do something
+//     }
   
-  })
-}
+//   })
+// }
 
+//this function checks the current booking status
 function Readbooking(){
-  
   firebase.firestore()
   .collection('SaracaHall')
   .doc('Machine1')
@@ -57,7 +84,7 @@ function Readbooking(){
   .doc('bookstatus')
   .get()
   .then( documentSnapshot =>{
-    var isbooked=documentSnapshot.get('isbooked?')
+    var isbooked=documentSnapshot.get('isbooked')
     console.log(isbooked)
     return (isbooked)
   })
@@ -69,4 +96,26 @@ function Readbooking(){
 
 
 
-
+//Check if machine is avail. if available, book it.
+  function checktrue(){
+    firebase.firestore()
+    .collection('SaracaHall')
+    .doc('Machine1')
+    .collection('bookstatus')
+    .doc('bookstatus')
+    .get()
+    .then( documentSnapshot =>{
+      var isbooked=documentSnapshot.get('isbooked')
+      if (isbooked == true){
+        console.log('machine is currently booked')
+        //do something
+      }
+      if(isbooked == false){
+        bookmachine1()
+        console.log('machine1 booked successfully')
+        //do something
+      }
+    
+    })
+  }
+  
